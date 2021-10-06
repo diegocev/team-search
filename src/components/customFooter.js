5838c85f491ca5dec7aa5295593802ff7a6b06a5
diegocev <diegocev@yahoo.com>
2021-10-06 12:00:59 -0500

    Initial Version 1.0

---
+++ src/components/customFooter.js
@@ -0,0 +1,11 @@
+
+import styles from './footer.module.css';
+
+export default function PageFooter(props) {
+
+    return (
+        <div className={styles.header}>
+            Pie de Pagina
+        </div>
+    );
+}
---
+++ src/components/pageHeader.js
@@ -0,0 +1,13 @@
+
+import styles from './pageHeader.module.css';
+
+export default function PageHeader(props) {
+    const { title } = props;
+    //console.log(styles);
+    return (
+        <div>
+            <h1 className={styles.header}>{title || 'Team Search'}</h1>
+            <h2 className={styles.subtitle}>Subtitle</h2>
+        </div>
+    );
+}
